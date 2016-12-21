d904ff238482df181532f675ec9df674423e3b6b
bobans@ <bobans@inbox.ru>
2016-12-21 17:04:00 +0300

    version 2 release

---
+++ cfg/mail.cfg.php
@@ -0,0 +1,8 @@
+<?php
+    $smtp_server = адрес SMTP - сервера(ssl://smtp.yandex.ru)
+    $smtp_port = порт SMTP - сервера(465)
+    $smtp_user = Имя пользователя для авторизации на SMTP - сервере
+    $smtp_password = Пароль для авторизации на SMTP - сервере
+    $reciever = Получатель сообщения
+    $subject = Тема сообщения
+
--- index.php
+++ index.php
@@ -10,20 +10,19 @@ $delay = 0; // Задержка между прыжками
 $max_jumping = 500; //Максимальное количество прыжков
 $start = microtime(true);
 
-$last_url='';
+$last_url = '';
 $last_id_from_file = 0;
 $last_id = 9999999999;
-$last_max_id=0;
+$last_max_id = 0;
 $count = 0;
 $msg_body = '';
 $lastid_content = file_get_contents('cfg/lastid.txt');
 $words_for_seach = explode(',', file_get_contents('cfg/words_for_search.txt'));
 
 
-if ($lastid_content != null && intval($lastid_content)>0)
-{
+if ($lastid_content != null && intval($lastid_content) > 0) {
     $last_id_from_file = $lastid_content;
-    $last_url='?lastid=' . (intval($last_id_from_file)) . 'ajax_id=' . $count . '&n=5';
+    $last_url = '?lastid=' . (intval($last_id_from_file)) . 'ajax_id=' . $count . '&n=5';
 }
 
 
@@ -48,17 +47,17 @@ while ($last_id > $last_id_from_file) {
                 if ((mb_strpos(mb_strtoupper($question), mb_strtoupper($line), 0, 'UTF-8') == true)
                     && mb_strpos(mb_strtoupper($msg_body), mb_strtoupper($question), 0, 'UTF-8') != true
                 ) {
-                    $msg_body.=  '<br />';
-                    $msg_body.=  'Вопрос: ' . $question . '<br />';
-                    $msg_body.=  'Категория: ' . $answers->{'catname'} . '<br />';
-                    $msg_body.=  'Link: https://otvet.mail.ru/question/' . $answers->{'id'} . '<br />';
-                    $msg_body.=  '<br />';
+                    $msg_body .= '<br />';
+                    $msg_body .= 'Вопрос: ' . $question . '<br />';
+                    $msg_body .= 'Категория: ' . $answers->{'catname'} . '<br />';
+                    $msg_body .= 'Link: https://otvet.mail.ru/question/' . $answers->{'id'} . '<br />';
+                    $msg_body .= '<br />';
                 }
             }
         }
     }
 
-    if ($last_max_id<$last_id) $last_max_id=$last_id;
+    if ($last_max_id < $last_id) $last_max_id = $last_id;
 
     if ($count == 1) {
         $last_id_ = $last_id;
@@ -70,12 +69,12 @@ while ($last_id > $last_id_from_file) {
 
 file_put_contents('cfg/lastid.txt', $last_max_id);
 $time = microtime(true) - $start;
-echo '<br>Количество слов в массиве для поиска:'.count($words_for_seach);
+echo '<br>Количество слов в массиве для поиска:' . count($words_for_seach);
 printf('<br>Время выполнялся %.2F сек.', $time);
 
 if ($msg_body <> '') {
     echo '<br>----------------------------------RESULT---------------------------------- <br>';
     echo $msg_body;
-    $msg_body.='<br> Время выполнения:'.round($time,2) .' сек.';
+    $msg_body .= '<br> Время выполнения:' . round($time, 2) . ' сек.';
     MailSmtp($msg_body);
 }
